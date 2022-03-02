dee48eb70bf192e28bca534ece47512c7d68284b
Mateusz Mędrek <luigimario1913@gmail.com>
2022-03-02 22:21:21 +0100

    chore: docs with docusaurus

---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,46 @@
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import React from 'react';
+
+import { HomepageFeatures } from '../components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+const HomepageHeader: React.FC = () => {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/guides">
+            Getting Started
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const Home: React.FC = () => {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+};
+
+export default Home;
