381ac50b2b66eb5a71511ae5fac6d221632840bd
Mateusz Mędrek <luigimario1913@gmail.com>
2022-03-13 19:04:35 +0100

    chore: add kotlin to example project

---
+++ packages/mobile/android/app/src/main/java/com/example/reactnativeavoidsoftinput/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.reactnativeavoidsoftinput
+
+import com.facebook.react.ReactActivity
+import android.os.Bundle
+import com.facebook.react.ReactActivityDelegate
+import com.zoontek.rnbootsplash.RNBootSplash
+
+class MainActivity : ReactActivity() {
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String? {
+        return "AvoidSoftinputExample"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return object : ReactActivityDelegate(this, mainComponentName) {
+            override fun loadApp(appKey: String) {
+                RNBootSplash.init(this@MainActivity)
+                super.loadApp(appKey)
+            }
+        }
+    }
+}
