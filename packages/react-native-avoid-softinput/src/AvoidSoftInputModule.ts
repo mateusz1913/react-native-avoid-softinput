08a20cb2aefdcf66c69580a6fe45a3f65553dddb
Mateusz Mędrek <mateusz.medrek@netguru.com>
2022-12-08 22:11:47 +0100

    refactor(#118): global properties to react-native stable APIs
    - remove global.__turboModuleProxy
    - remove global.nativeFabricUIManager

--- packages/react-native-avoid-softinput/src/AvoidSoftInputModule.ts
+++ packages/react-native-avoid-softinput/src/AvoidSoftInputModule.ts
@@ -1,17 +1,5 @@
-import { NativeModules } from 'react-native';
-
 import type { AvoidSoftInputNativeModuleType } from './types';
 
-declare global {
-  // eslint-disable-next-line no-var, no-underscore-dangle
-  var __turboModuleProxy: unknown;
-}
-
-// eslint-disable-next-line no-underscore-dangle
-const isTurboModuleEnabled = global.__turboModuleProxy !== null;
-
-const myModule: AvoidSoftInputNativeModuleType = isTurboModuleEnabled
-  ? require('./NativeAvoidSoftInputModule').default
-  : NativeModules.AvoidSoftInput;
-
+const myModule: AvoidSoftInputNativeModuleType = require('./NativeAvoidSoftInputModule').default;
+  
 export default myModule;
--- packages/react-native-avoid-softinput/src/NativeAvoidSoftInputView.ts
+++ packages/react-native-avoid-softinput/src/NativeAvoidSoftInputView.ts
@@ -1,17 +1,7 @@
 import type { HostComponent } from 'react-native';
-import { requireNativeComponent } from 'react-native';
 
 import type { AvoidSoftInputViewProps } from './types';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var nativeFabricUIManager: unknown;
-}
-
-const isFabricEnabled = global.nativeFabricUIManager !== null;
-
-const component: HostComponent<AvoidSoftInputViewProps> = isFabricEnabled
-  ? require('./AvoidSoftInputViewNativeComponent').default
-  : requireNativeComponent('AvoidSoftInputView');
+const component: HostComponent<AvoidSoftInputViewProps> = require('./AvoidSoftInputViewNativeComponent').default;
 
 export default component;
