d77e61ae1a8012b6ecf543fa78e06c89ce90f99b
Abuzar Mirza <abuzar.mirza+1@easyeat.ai>
2023-07-01 14:45:47 +0530

    fix: auto import components

--- packages/react-native-avoid-softinput/src/index.tsx
+++ packages/react-native-avoid-softinput/src/index.tsx
@@ -1,4 +1,6 @@
-export * from './AvoidSoftInput';
-export * from './AvoidSoftInputView';
-export * from './hooks';
-export * from './types';
+import AvoidSoftInput from "./AvoidSoftInput";
+import AvoidSoftInputView from "./AvoidSoftInputView";
+export * from "./hooks";
+export * from "./types";
+
+export { AvoidSoftInput, AvoidSoftInputView };
