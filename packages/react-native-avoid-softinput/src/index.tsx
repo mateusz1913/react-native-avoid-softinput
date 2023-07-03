6836cf134a06fd739586c32c76e21a0aaff8c79c
abdemirza <45914394+abdemirza@users.noreply.github.com>
2023-07-03 19:36:33 +0530

    fix: auto import components (#169)
    * fix: auto import components
    
    * lint: fixed no default export issue
    
    * fix: fixed linting issue

--- packages/react-native-avoid-softinput/src/index.tsx
+++ packages/react-native-avoid-softinput/src/index.tsx
@@ -1,4 +1,8 @@
+import { AvoidSoftInput } from './AvoidSoftInput';
+import { AvoidSoftInputView } from './AvoidSoftInputView';
 export * from './AvoidSoftInput';
 export * from './AvoidSoftInputView';
 export * from './hooks';
 export * from './types';
+
+export { AvoidSoftInput, AvoidSoftInputView };
