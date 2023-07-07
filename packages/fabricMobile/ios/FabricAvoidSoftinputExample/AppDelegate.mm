006c664fa42c4d569fdc0ae19976ca96eda166aa
Mateusz Mędrek <mateusz.medrek@callstack.com>
2023-07-07 11:14:30 +0200

    chore: upgrade deps & limit support to latest 3 RN versions

--- packages/fabricMobile/ios/FabricAvoidSoftinputExample/AppDelegate.mm
+++ packages/fabricMobile/ios/FabricAvoidSoftinputExample/AppDelegate.mm
@@ -9,6 +9,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.moduleName = @"FabricAvoidSoftinputExample";
+    // You can add your custom initial props in the dictionary below.
+    // They will be passed down to the ViewController used by React Native.
+    self.initialProps = @{};
+
     [super application:application didFinishLaunchingWithOptions:launchOptions];
     [RNBootSplash initWithStoryboard:@"BootSplash" rootView:self.window.rootViewController.view];
     return YES;
@@ -21,16 +25,6 @@
 #else
     return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
-}
-
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-    return true;
 }
 
 @end
