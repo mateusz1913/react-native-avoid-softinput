704d632e097bce548a15239a4b76d9296ef308e6
Mateusz Mędrek <mateusz.medrek@callstack.com>
2025-08-15 16:22:34 +0200

    fix(#238): build issues on RN81, drop support for RN < 78

--- packages/react-native-avoid-softinput/android/src/newarch/java/com/reactnativeavoidsoftinput/AvoidSoftInputViewManager.kt
+++ packages/react-native-avoid-softinput/android/src/newarch/java/com/reactnativeavoidsoftinput/AvoidSoftInputViewManager.kt
@@ -1,8 +1,6 @@
 package com.reactnativeavoidsoftinput
 
-import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.module.annotations.ReactModule
-import com.facebook.react.uimanager.BaseViewManagerDelegate
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 import com.facebook.react.viewmanagers.AvoidSoftInputViewManagerInterface
@@ -16,60 +14,14 @@ import com.reactnativeavoidsoftinput.events.AvoidSoftInputShownEvent
 @ReactModule(name = AvoidSoftInputView.NAME)
 class AvoidSoftInputViewManager :
     ReactViewManager(), AvoidSoftInputViewManagerInterface<AvoidSoftInputView> {
-    private val delegate =
-        object : BaseViewManagerDelegate<ReactViewGroup, AvoidSoftInputViewManager>(this) {
-            override fun setProperty(view: ReactViewGroup, propName: String, value: Any?) {
-                when (propName) {
-                    "avoidOffset" ->
-                        mViewManager.setAvoidOffset(
-                            view as AvoidSoftInputView,
-                            (value as Double?)?.toFloat() ?: 0f
-                        )
-                    "easing" -> mViewManager.setEasing(view as AvoidSoftInputView, value as String?)
-                    "enabled" ->
-                        mViewManager.setEnabled(
-                            view as AvoidSoftInputView,
-                            value as Boolean? ?: true
-                        )
-                    "hideAnimationDelay" ->
-                        mViewManager.setHideAnimationDelay(
-                            view as AvoidSoftInputView,
-                            (value as Double?)?.toInt() ?: 300
-                        )
-                    "hideAnimationDuration" ->
-                        mViewManager.setHideAnimationDuration(
-                            view as AvoidSoftInputView,
-                            (value as Double?)?.toInt() ?: 220
-                        )
-                    "showAnimationDelay" ->
-                        mViewManager.setShowAnimationDelay(
-                            view as AvoidSoftInputView,
-                            (value as Double?)?.toInt() ?: 0
-                        )
-                    "showAnimationDuration" ->
-                        mViewManager.setShowAnimationDuration(
-                            view as AvoidSoftInputView,
-                            (value as Double?)?.toInt() ?: 660
-                        )
-                    else -> super.setProperty(view, propName, value)
-                }
-            }
-
-            override fun receiveCommand(
-                view: ReactViewGroup,
-                commandName: String,
-                args: ReadableArray?
-            ) {
-                super.receiveCommand(view, commandName, args)
-            }
-        }
+    private val delegate = AvoidSoftInputViewManagerDelegate(this)
 
     override fun getName() = AvoidSoftInputView.NAME
 
     override fun getDelegate() = delegate
 
-    override fun createViewInstance(reactContext: ThemedReactContext): AvoidSoftInputView {
-        return AvoidSoftInputView(reactContext)
+    override fun createViewInstance(context: ThemedReactContext): AvoidSoftInputView {
+        return AvoidSoftInputView(context)
     }
 
     override fun prepareToRecycleView(
---
+++ packages/react-native-avoid-softinput/android/src/reactnative77/java/com/reactnativeavoidsoftinput/AvoidSoftInputViewDelegate.kt
@@ -0,0 +1,46 @@
+package com.reactnativeavoidsoftinput
+
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.uimanager.BaseViewManagerDelegate
+import com.facebook.react.views.view.ReactViewGroup
+
+class AvoidSoftInputViewManagerDelegate(viewManager: AvoidSoftInputViewManager) :
+    BaseViewManagerDelegate<ReactViewGroup, AvoidSoftInputViewManager>(viewManager) {
+    override fun setProperty(view: ReactViewGroup, propName: String, value: Any?) {
+        when (propName) {
+            "avoidOffset" ->
+                mViewManager.setAvoidOffset(
+                    view as AvoidSoftInputView,
+                    (value as Double?)?.toFloat() ?: 0f
+                )
+            "easing" -> mViewManager.setEasing(view as AvoidSoftInputView, value as String?)
+            "enabled" ->
+                mViewManager.setEnabled(view as AvoidSoftInputView, value as Boolean? ?: true)
+            "hideAnimationDelay" ->
+                mViewManager.setHideAnimationDelay(
+                    view as AvoidSoftInputView,
+                    (value as Double?)?.toInt() ?: 300
+                )
+            "hideAnimationDuration" ->
+                mViewManager.setHideAnimationDuration(
+                    view as AvoidSoftInputView,
+                    (value as Double?)?.toInt() ?: 220
+                )
+            "showAnimationDelay" ->
+                mViewManager.setShowAnimationDelay(
+                    view as AvoidSoftInputView,
+                    (value as Double?)?.toInt() ?: 0
+                )
+            "showAnimationDuration" ->
+                mViewManager.setShowAnimationDuration(
+                    view as AvoidSoftInputView,
+                    (value as Double?)?.toInt() ?: 660
+                )
+            else -> super.setProperty(view, propName, value)
+        }
+    }
+
+    override fun receiveCommand(view: ReactViewGroup, commandName: String, args: ReadableArray?) {
+        super.receiveCommand(view, commandName, args)
+    }
+}
